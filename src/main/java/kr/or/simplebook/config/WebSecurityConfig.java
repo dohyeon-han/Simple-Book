585a9e4107bc6ae98ab0810a9c94b86c7ea62217
dohyeon-han <limited.hyeon@gmail.com>
2021-11-11 22:44:06 +0900

    Add AuthenticationProvider in security config

--- src/main/java/kr/or/simplebook/config/WebSecurityConfig.java
+++ src/main/java/kr/or/simplebook/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package kr.or.simplebook.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration // 설정 파일, bean 등록
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private CustomAuthenticationProvider authProvider;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -27,10 +34,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable() //
-				.authorizeRequests().antMatchers("/login*").permitAll()// login은 모든 사용자 접근 가능
-				.anyRequest().authenticated()
+				.authorizeRequests()
+				.antMatchers("/login*")
+				.permitAll()// login은 모든 사용자 접근 가능
+				.anyRequest()
+				.authenticated()
 			.and()
-				.formLogin().loginPage("/login") // login.jsp
+				.formLogin()
+				.loginPage("/login") // login.jsp
 				.loginProcessingUrl("/doLogin") // post url
 				.defaultSuccessUrl("/") // 성공 후 url
 				.usernameParameter("id").passwordParameter("pw") // id, pw에 대한 파라미터
@@ -38,18 +49,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.failureHandler(new LoginFailureHandler())
 				.permitAll()
 			.and()
-				.logout().logoutUrl("/doLogout") // logout 요청 url
+				.logout()
+				.logoutUrl("/doLogout") // logout
 				.logoutSuccessUrl("/login") // logout 후 url
 				.invalidateHttpSession(true) // 세션 삭제
 				.deleteCookies("true") // 쿠키 삭제
 			.and()
-				.exceptionHandling().accessDeniedPage("/login");// 접근 권한이 없을 때 접근 시 이동
+				.exceptionHandling()
+				.accessDeniedPage("/login");// 접근 권한이 없을 때 접근 시 이동
 	}
 
 	@Override
 	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-				.and().withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER").and()
-				.withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+		auth.authenticationProvider(authProvider);
 	}
 }
