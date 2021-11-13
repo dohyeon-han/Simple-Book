c92e96f6c32fd244d017b0bd5ea85b431239d371
dohyeon-han <limited.hyeon@gmail.com>
2021-11-13 23:33:26 +0900

    Modify app config ComponentScan

--- src/main/java/kr/or/simplebook/config/ApplicationConfig.java
+++ src/main/java/kr/or/simplebook/config/ApplicationConfig.java
@@ -3,10 +3,12 @@ package kr.or.simplebook.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 @Configuration
-@ComponentScan(basePackages = {"kr.or.simplebook.dao", "kr.or.simplebook.service", "kr.or.simplebook.config"})
-@Import({WebSecurityConfig.class, DBconfig.class})
+@ComponentScan(basePackages = {"kr.or.simplebook.dao",
+		"kr.or.simplebook.config","kr.or.simplebook.serviceImpl"})
+@Import({WebSecurityConfig.class, DBconfig.class, WebSecurity.class})
 public class ApplicationConfig {
 
 }
